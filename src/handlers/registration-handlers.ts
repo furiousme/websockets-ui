65282786fbd13f6a50503e098097f8d263931cc1
furiousme <shuptaroks@gmail.com>
2024-10-28 23:54:31 +0200

    feat: add basic validation

--- src/handlers/registration-handlers.ts
+++ src/handlers/registration-handlers.ts
@@ -11,6 +11,17 @@ export const registerPlayerHandler = (ws: FIXME, socketId: string, payload: FIXM
   const { name, password } = parsedData;
   const client = clients.get(socketId);
 
+  const nameExists = Array.from(players.values()).some((el) => el.name === name);
+
+  if (nameExists) {
+    sendSocketMessage(client, Command.REG, {
+      error: true,
+      errorText: 'This name is already taken',
+    });
+
+    return;
+  }
+
   players.set(socketId, { name, password, socketId });
   const responseData = {
     name,
@@ -18,6 +29,7 @@ export const registerPlayerHandler = (ws: FIXME, socketId: string, payload: FIXM
     error: false,
     errorText: '',
   };
+
   sendSocketMessage(ws, Command.REG, responseData);
   notifyClientsAboutRoomUpdates();
   notifyClientsAboutWinnersUpdates([client]);
