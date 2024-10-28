cfc6d1eab94c74f1196ead4a5640321b0501d2ff
furiousme <shuptaroks@gmail.com>
2024-10-28 19:34:53 +0200

    lint: setup ts and linters

---
+++ webpack.config.mjs
@@ -0,0 +1,25 @@
+import path from 'node:path';
+import nodeExternals from 'webpack-node-externals';
+
+export default {
+  entry: './index.js',
+  target: 'node',
+  mode: 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.cjs',
+    path: path.resolve('./dist'),
+  }
+};
